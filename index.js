214981d1b1fec0c659ff2887288e9f559b7f30fa
Karen Neirot <karenneirot@gmail.com>
2023-02-22 23:16:56 -0300

    tareas

---
+++ index.js
@@ -0,0 +1,33 @@
+const titulo="un mensaje"; 
+console.log(titulo)
+
+let primera = 3; segunda = 5; resultado =(primera + segunda)
+ console.log(resultado)
+
+let usuario ="hola nombre de usuario"; 
+console.log(usuario)
+
+let flor = 10;
+let rosa = 20;
+let total =(flor + rosa)
+console.log(total)
+
+let conejo_a = 2;
+let conejo_b = 8;
+
+if(conejo_a > conejo_b){console.log("conejo_a es mayor")}
+else{console.log("conejo_b es mayor")}
+
+let numeroA = 12;
+let numeroB = 9;
+let numeroC = 23;
+if(numeroA > numeroB && numeroA < numeroC){console.log("numeroC es el mayor")}
+else{console.log("la condicion es falsa")}
+
+let auto = 8;
+if( auto % 2 === 0){console.log("el numero es divisible en 2")}
+else{console.log("el numero no es divisible")}
+
+let bicicleta = 10;
+if(bicicleta % 2 || bicicleta % 3 ||  bicicleta % 5 || bicicleta % 7){console.log("true")}
+else{console.log("false")}
